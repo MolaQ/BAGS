2576b6856fca1c27a8fb3dbf07241f1002fe6c81
MolaQ <marcin.molak@gmail.com>
2022-04-03 12:36:22 +0200

    Stage | Points

--- resources/views/livewire/resultstable.blade.php
+++ resources/views/livewire/resultstable.blade.php
@@ -1,12 +1,41 @@
 <div>
-    @foreach ($results as $result)
-   {{ $result->team->teammembers}} |
 
-   @for ($i = 1; $i <= $ilezadan; $i++)
-   Zad.{{ $i }}: {{ $punkty[$result->team_id][$i]/100 }} |
-@endfor
 
-{{ $result->sum/100 }}  <br>
-    @endforeach
+
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+              <th scope="col"></th>
+              <th scope="col"></th>
+              <th scope="col" colspan="{{ $ilezadan }}">Punktacja za zadania</th>
+              <th scope="col"></th>
+            </tr>
+            <tr>
+                <th scope="col">Poz</th>
+                <th scope="col">Zespół</th>
+
+                    @for ($i = 1; $i <= $ilezadan; $i++)
+                        <th>{{ $i }}</th>
+                    @endfor
+
+                <th scope="col">Suma punktów</th>
+              </tr>
+          </thead>
+          <tbody>
+            @foreach ($results as $result)
+            <tr>
+              <th scope="row">{{ $loop->iteration }}</th>
+              <td>{{ $result->team->teammembers}} ({{ $result->team->classname }} - {{ $result->team->group }})</td>
+                @for ($i = 1; $i <= $ilezadan; $i++)
+                    <th> {{ $punkty[$result->team_id][$i]/100 }}</th>
+                @endfor
+
+              <td>{{ $result->sum/100 }}</td>
+            </tr>
+            @endforeach
+
+
+      </table>
+
 </div>
 
